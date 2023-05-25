7e09c28d6323a6384d551de59f8e235e2d737137
EashanPatel1 <116560136+EashanPatel1@users.noreply.github.com>
2023-05-25 16:46:22 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,6 @@
+function add() {
+    var result =
+    Number(document.getElementById("n1").value) +
+    Number(document.getElementById("n2").value);
+    document.getElementById("ans").innerHTML=result;
+}
